a8f089863552e0b1e3863c4826c80587b20b33f0
Mark Ganhao <mark.ganhao@gmail.com>
2020-10-20 01:07:13 -0400

    deplay test 7

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/api/v1/admin/addProject', async (req, res) => {
 })
 
 if (process.env.NODE_ENV === "production)") {
-  app.use(express.static('client/build'));  // path.join(__dirname, "client/build"))
+  app.use(express.static(path.join(__dirname, "client/build")));  
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
