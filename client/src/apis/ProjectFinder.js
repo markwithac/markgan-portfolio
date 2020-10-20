084e372572058c1beb785737635e31cbe36959c0
Mark Ganhao <mark.ganhao@gmail.com>
2020-10-20 01:24:14 -0400

    baseUR L  change

--- client/src/apis/ProjectFinder.js
+++ client/src/apis/ProjectFinder.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const development = process.env.NODE_ENV
+const development = !process.env.NODE_ENV
 
 export default axios.create(
   development ? { baseURL: "http://localhost:3001/api/v1" } : { baseULR: ""}
