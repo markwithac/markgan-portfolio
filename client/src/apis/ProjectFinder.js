f6c21f2241277bf9d4952cdbe9c028e0d67dd425
Mark Ganhao <mark.ganhao@gmail.com>
2020-11-22 16:22:32 -0500

    Articles Deploy v1

--- client/src/apis/ProjectFinder.js
+++ client/src/apis/ProjectFinder.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:3001' 
+  baseURL: "https://markgan-portfolio.herokuapp.com/" || 'http://localhost:3001' 
 });
 
-// "https://markgan-portfolio.herokuapp.com/" || 
