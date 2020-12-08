81f6a1cf440d1d1bf79449bf3801eac103d2c718
unknown <mark.ganhao@gmail.com>
2020-12-07 19:02:28 -0500

    about page updates

--- client/src/apis/ProjectFinder.js
+++ client/src/apis/ProjectFinder.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:3001' 
+  baseURL: "https://markgan-portfolio.herokuapp.com/" || 'http://localhost:3001' 
 });
 
 // "https://markgan-portfolio.herokuapp.com/" || 
